820dd5f702a223702f3e0da89da0e4f1b8bca43f
newtalder <25nalder@go.dsdmail.net>
2023-05-01 12:52:17 -0600

    main

--- fern.py
+++ fern.py
@@ -1,4 +1,4 @@
 class Fern():
     def talk(self):
         print("You see a fern that is so full that it sags, it does not turn to look at you. Maybe lighten it's load?")
-fern = Fern()
+ferny = Fern()
