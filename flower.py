527ce0e1422462710236a37f5cf42fa882fc7a8f
newtalder <25nalder@go.dsdmail.net>
2023-05-03 12:25:16 -0600

    gelp????

--- flower.py
+++ flower.py
@@ -4,7 +4,7 @@ class Flower():
         self.inventory = []
     def talk(self):
         print(f"Orchid?: *Growwwllll*\nYou give a thumbs up, almost as to ask if it's ok.\nThe orchid shakes its sad, droopy head.")
-    def get_item(self, player):
+    def get_item(self):
         flowchoi = input("You look around the room to see dead flowers of all kinds, all dried up. In the left corner of the room, there lies a watering can, would you like to pick it up?\n")
         if flowchoi in ("Yes", "Y", "y", "yes"):
             pick = input("What item would you like to 'get'?").lower()
