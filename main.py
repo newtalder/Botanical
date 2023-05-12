947f8e22de1676f830ddb924f941edd08e93e058
Newt <112032472+newtalder@users.noreply.github.com>
2023-05-12 22:17:00 +0000

    main

--- main.py
+++ main.py
@@ -157,20 +157,20 @@ if finalflower == 3:
 			time.sleep(1.3)
 			print("The plant lets out a horrific shriek, which concerns you even more.")
 			time.sleep(1.3)
-			speakopt = ("")
-			while speakopt not in ("3"):
+			speakopt = ""
+			while speakopt != "3":
 				speakopt = input("What will you say to the plant?\nOPTIONS:\n1: Ask the plant if it's lonely\n2: Ask the plant if it's hurt\n3: Ask the plant if it's hungry\n")
-				if speakopt in ("1"):
+				if speakopt == "1":
 					print(f"{name}: Are you lonely?")
 					time.sleep(1.3)
 					print("The plant shakes its head.")
 					continue
-				if speakopt in ("2"):
+				elif speakopt == "2":
 					print(f"{name}: Are you hurt?")
 					time.sleep(1.3)
 					print("The plant shakes its head.")
 					continue
-				elif speakopt in ("3"):
+				elif speakopt == "3":
 					print(f"{name}: Are you hungry?")
 					time.sleep(1.3)
 					print("The plant, Venus, nods sadly.")
@@ -184,12 +184,12 @@ if finalflower == 3:
 					print("The plant nods enthusiastically.")
 					time.sleep(1.3)
 					choosing = ("")
-					while choosing not in ("A"):
+					while choosing != "B":
 						choosing = input("What will you try to do?\nOPTIONS:\nA: Try to open the windows\nB: Ask the plants for help\n")
-						if choosing in ("A"):
+						if choosing == "A":
 							print("You try to open the windows, but they won't budge without help.")
 							continue
-						elif choosing in ("B"):
+						elif choosing == "B":
 							print("Orchid: Hey! You called?")
 							time.sleep(1.3)
 							print("Fern: What's up!")
